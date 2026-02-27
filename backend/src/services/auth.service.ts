0de76bda6954e5869a83f0cdbfb0cb1c6e434635
diegoseg15 <diegofersv@gmail.com>
2026-02-27 13:43:07 -0500

    :necktie: implementar login con validación de credenciales y generación de JWT

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { signToken } from "../config/jwt";
+import { LoginDTO } from "../models/login.dto";
 import { RegisterDTO } from "../models/regsiter.dto";
 import { UserRepository } from "../repositories/user.repository";
 import bcrypt from "bcryptjs";
@@ -22,5 +24,36 @@ export class AuthService {
     });
 
     return newUser;
+  }
+
+  async login(data: LoginDTO) {
+    const user = await this.userRepository.findByEmail(data.email);
+    if (!user) {
+      const error: any = new Error("Las credenciales son invalidas");
+      error.status = 401;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+    if (!isPasswordValid) {
+      const error: any = new Error("Las credenciales son invalidas");
+      error.status = 401;
+      throw error;
+    }
+
+    const token = signToken({
+      userId: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    const safeUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    return { user: safeUser, token };
   }
 }
