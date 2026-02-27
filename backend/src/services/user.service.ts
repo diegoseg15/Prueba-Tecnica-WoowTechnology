00f29eeee87ed55fec5f509942f73dfb014c6ee5
diegoseg15 <diegofersv@gmail.com>
2026-02-27 15:46:26 -0500

    :necktie: agregar método getAllUsers con paginación y cálculo de totalde páginas

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -26,4 +26,21 @@ export class UserService {
 
     return updatedUser;
   }
+
+  async getAllUsers(page: number, limit: number = 10) {
+    const offset = (page - 1) * limit;
+
+    const users = await this.userRepository.findAll(limit, offset);
+    const total = await this.userRepository.countAll();
+
+    return {
+      users,
+      pagination: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
+  }
 }
