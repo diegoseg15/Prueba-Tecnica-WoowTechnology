76d5b8400931d34b2cb8bed0f9e0386d9a1d52f4
diegoseg15 <diegofersv@gmail.com>
2026-02-27 14:00:03 -0500

    :building_construction: conectar rutas protegidas de usuarios al servidor Express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import usersRoutes from "./routes/users.routes";
 
 dotenv.config();
 
 const app = express();
+const path = "/api";
+
 app.use(express.json());
 app.use(cors());
 
@@ -15,7 +18,8 @@ app.get("/", (_req, res) => {
   });
 });
 
-app.use("/api/auth", authRoutes);
+app.use(`${path}/auth`, authRoutes);
+app.use(`${path}/users`, usersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
