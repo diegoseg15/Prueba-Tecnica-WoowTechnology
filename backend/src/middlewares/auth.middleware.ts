400ad43dad14c8ff8d826fe910c88d6988cd7714
diegoseg15 <diegofersv@gmail.com>
2026-02-27 13:56:33 -0500

    :passport_control: agregar authMiddleware con validación de token Bearer

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { AuthenticatedRequest } from "../types/express";
+
+export const authMiddleware = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        message: "No autenticado. Token requerido.",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      userId: string;
+      email: string;
+      role: "user" | "admin";
+    };
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token inválido o expirado",
+    });
+  }
+};
